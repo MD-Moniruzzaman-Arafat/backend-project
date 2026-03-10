023a5fbaa3417b7c4e3fc7c75e922e4623786684
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-10 14:25:08 +0600

    ekhane amra fs module use kore file access kore basic get api diye data gulo access korci

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+const fs = require('fs').promises;
 const express = require('express');
 const app = express();
 const port = 3000;
+
+async function readMyFile() {
+  try {
+    const data = await fs.readFile(`${__dirname}/data/data.json`, 'utf8');
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+app.get('/api/v1/tours', async (req, res) => {
+  const result = await readMyFile();
+  res.status(200).json({
+    status: 'success',
+    results: JSON.parse(result).length,
+    data: {
+      tours: JSON.parse(result),
+    },
+  });
+});
 
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'success', message: 'Hi This Is My Project' });
