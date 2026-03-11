2f345ada6fe6097f930f77603ae987d3f29aee24
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-11 15:12:25 +0600

    amra ekhon mongodb er library mongoose install ang connect korsi

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 require('dotenv').config();
 const app = require('./app');
+const mongoose = require('mongoose');
 
 console.log(process.env.NODE_ENV);
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DB);
+    console.log(`✅ MongoDB Connect: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`❌ MongoDB Not Connect: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 const port = process.env.PORT || 8000;
 
