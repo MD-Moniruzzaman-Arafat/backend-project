c6e566ad602710ade95a3d4bab6fd11580d19452
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-11 16:08:23 +0600

    ekhane amra basic model create korsi and database ee save korsi

--- server.js
+++ server.js
@@ -21,6 +21,37 @@ const connectDB = async () => {
 
 connectDB();
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Sea Explorer',
+  rating: 5.7,
+  price: 300,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log(err));
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
