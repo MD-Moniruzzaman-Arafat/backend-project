b212a8787bdcaa69d9095de76bde5f90792ef398
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-11 23:32:26 +0600

    ekhane amra tour model ee aro data field add korlam and amra new tour post kore db te pathaisi

--- model/tourModel.js
+++ model/tourModel.js
@@ -3,17 +3,54 @@ const { default: mongoose } = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'A tour must have a name'],
+    required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a durations'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    require: [true, 'A tour must have a price'],
+    required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
