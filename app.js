1f7ca1b6ad5264a9b5d34e856589e3fa917f7767
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-22 16:36:03 +0600

    ekhane amra global error handler add korsi jar parameter 4ta thakbe then express automatic bujbe eta global eror handler and eta sobar last ee thakbe

--- app.js
+++ app.js
@@ -31,10 +31,27 @@ app.get('/', (req, res) => {
   res.status(200).json({ status: 'success', message: 'Hi This Is My Project' });
 });
 
-app.all('*splat', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+// handle unhandled route
+app.all('*splat', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.statusCode = 404;
+  err.status = 'fail';
+
+  next(err);
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 module.exports = app;
