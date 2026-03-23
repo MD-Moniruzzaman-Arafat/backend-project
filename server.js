698667cc574533cb92aeba0a8ca28c9f9984ccf0
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-23 16:10:15 +0600

    ekhane amra express er bahire synchronous error gulo dhorbo like variable undefine nul propery etc

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION! Shutting down...');
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
 require('dotenv').config();
 const app = require('./app');
 const mongoose = require('mongoose');
