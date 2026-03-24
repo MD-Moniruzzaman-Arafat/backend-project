062bdc56d08e53d453b651bda1cf01a9fd0b679a
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-24 15:42:56 +0600

    ekhane mara jwt use korsi jokhon use create hobe tar sate akta token jenarate hobe

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,28 @@
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
+
+const signToken = (id) => {
+  return jwt.sign(
+    { id }, // payload
+    process.env.JWT_SECRET, // secret key
+    { expiresIn: process.env.JWT_EXPIRES_IN } // expire
+  );
+};
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       newUser,
     },
